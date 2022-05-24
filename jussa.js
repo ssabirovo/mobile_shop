0eebd54d79b04224c00816eab2ce785711c20aa6
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-05-24 09:12:51 +0500

    js homework

--- jussa.js
+++ jussa.js
@@ -1,24 +1,59 @@
-let addIphone7 = document.getElementById("iPhone7");
-let temp = document.getElementById("cart");
-let plusCart = document.getElementById("plusCart");
-let cart = document.getElementById("cart");
+let star1 = document.getElementById("star1");
+let star2 = document.getElementById("star2");
+let star3 = document.getElementById("star3");
+let star4 = document.getElementById("star4");
+let star5 = document.getElementById("star5");
 
-plusCart.addEventListener("click", () => {
-  cart.classList.toggle("cart");
+star1.addEventListener("click", () => {
+  star1.classList.toggle("golden");
+  star2.classList.remove("golden");
+  star3.classList.remove("golden");
+  star4.classList.remove("golden");
+  star5.classList.remove("golden");
+});
+star2.addEventListener("click", () => {
+  star1.classList.toggle("golden");
+  star2.classList.toggle("golden");
+  star3.classList.remove("golden");
+  star4.classList.remove("golden");
+  star5.classList.remove("golden");
+});
+star3.addEventListener("click", () => {
+  star1.classList.toggle("golden");
+  star2.classList.toggle("golden");
+  star3.classList.toggle("golden");
+  star4.classList.remove("golden");
+  star5.classList.remove("golden");
+});
+star4.addEventListener("click", () => {
+  star1.classList.toggle("golden");
+  star2.classList.toggle("golden");
+  star3.classList.toggle("golden");
+  star4.classList.toggle("golden");
+  star5.classList.remove("golden");
+});
+star5.addEventListener("click", () => {
+  star1.classList.toggle("golden");
+  star2.classList.toggle("golden");
+  star3.classList.toggle("golden");
+  star4.classList.toggle("golden");
+  star5.classList.toggle("golden");
 });
 
-addIphone7.addEventListener("click", () => {
-  let newDiv = document.createElement("div");
-  let newImg = newDiv.createElement("img");
-  let h1 = newDiv.createElement("h1");
-  let h1_2 = newDiv.createElement("h1");
-  let button = newDiv.createElement("h1");
+// loader
+let start = document.getElementById("start");
+let loaderBlue = document.getElementById("loaderBlue");
 
-  newImg.src = "./IMAGES/iphone-7.png";
-  h1.innerHTML = "iPhone-7";
-  h1_2.innerHTML = "$10";
-  button.innerHTML = "remove";
-
-  newDiv.classList.add("count");
-  temp.appendChild(newDiv);
+start.addEventListener("click", () => {
+  loaderBlue.style.left = "0";
 });
+
+// gridcha
+
+let cells = document.querySelectorAll(".cell");
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("onmouseover", () => {
+    cells[i].style.backgroundColor = "red";
+  });
+}
